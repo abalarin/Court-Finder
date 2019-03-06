5cd8e957d88e91a2d5df41db24c5617fc1a74dc8
abalarin <abalarin@outlook.com>
2019-03-05 22:13:20 -0500

    remove gitignore & integrate google maps

--- CourtFinder/endpoints/courts/routes.py
+++ CourtFinder/endpoints/courts/routes.py
@@ -5,4 +5,8 @@ courts = Blueprint('courts', __name__)
 
 @courts.route('/courts')
 def list_courts():
-    return render_template('courts.html')
+    return render_template('courts/courts.html')
+
+@courts.route('/map')
+def map():
+    return render_template('courts/map.html')
