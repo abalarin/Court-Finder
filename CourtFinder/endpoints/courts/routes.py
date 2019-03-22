1e08d9f24110f0800f9f68a2797dd07ee2b3d6e1
= <=>
2019-03-21 21:00:35 -0400

    court and user pages

--- CourtFinder/endpoints/courts/routes.py
+++ CourtFinder/endpoints/courts/routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template
+from CourtFinder.models.courts import Court
 
 courts = Blueprint('courts', __name__)
 
 
 @courts.route('/courts')
 def list_courts():
-    return render_template('courts/courts.html')
+    court = Court.query.all()
+    return render_template('courts/courts.html', Courts=court)
+
 
 @courts.route('/map')
 def map():
     return render_template('courts/map.html')
+
--- CourtFinder/endpoints/users/routes.py
+++ CourtFinder/endpoints/users/routes.py
@@ -1,21 +1,31 @@
 from flask import Blueprint, render_template
-
+from CourtFinder.models.users import User
 users = Blueprint('users', __name__)
 
 
 @users.route('/profile')
 def profile():
-    username = "Narwhalian"
-    return render_template('users/profile.html', name=username)
+    user_name = "Narwhalian"
+    user = User.query.filter_by(username=user_name)
+    return render_template('users/profile.html', user=user)
 
 
 @users.route('/abalarin')
 def abalarin_route():
-    username = "abalarin"
-    return render_template('users/profile.html', name=username)
+    user_name = "abalarin"
+    user = User.query.filter_by(username=user_name)
+    return render_template('users/profile.html', user=user)
 
 
 @users.route('/mzacierka')
 def mzacierka_route():
-    username = "mzacierka"
-    return render_template('users/profile.html', name=username)
+    user_name = "mzacierka"
+    user = User.query.filter_by(username=user_name)
+    return render_template('users/profile.html', user=user)
+
+
+@users.route('/janedoe')
+def janedoe_route():
+    user_name = "janedoe"
+    user = User.query.filter_by(username=user_name).first()
+    return render_template('users/profile.html', user=user)
