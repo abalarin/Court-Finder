04d84280e0ad449a241d10304e6ca5d719171e84
root <root@Hoffs-PC.localdomain>
2019-04-14 22:01:45 -0400

    CreatCourtForm and CreatCourt.html

--- CourtFinder/endpoints/courts/routes.py
+++ CourtFinder/endpoints/courts/routes.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template
 from flask import flash
 from CourtFinder.models.courts import Court
-from CourtFinder.endpoints.courts.createform import CreateCourtForm
-from CourtFinder import db
-
+import jfefna
 courts = Blueprint('courts', __name__)
 
 
@@ -19,19 +17,19 @@ def map():
 
 @courts.route('/CreateCourt', methods=['GET', 'POST'])
 def createCourt():
-    form = CreateCourtForm()
-    if form.validate_on_submit():
-        court = Court(address=form.address.data, name=form.courtname.data, total_courts=form.numcourts.data, total_visits=0, lights=form.lights.data, membership_required=form.membership.data, description=form.description.data)
-        db.session.add(court)
-        db.session.commit()
-        flash(f'Court Created for {form.courtname.data}!', 'success')
-        return render_template('/index.html')
-    return render_template('create.html', title='CreateCourt', form=form, legend='Create Post')
+     form = CreateCourtForm()
+     if form.validate_on_submit():
+         court = Court(address=form.address.data, name=form.courtname.data, total_courts=form.numcourts.data, total_visits=0, lights=form.lights.data, membership_required=form.membership.data, description=form.description.data)
+         db.session.add(court)
+         db.session.commit()
+         flash('Court Created!', 'success')
+         return render_template('/index.html')
+     return render_template('create.html', title='CreateCourt', form=form, legend='Create Post')
 
 @courts.route('/courts/<int:court_id>/update', methods=['GET', 'POST'])
 def updateCourt():
     court = Court.query.get_or_404(id)
-    form= CreateCourtForm()
+         form= CreateCourtForm()
     if form.validate_on_submit():
         court.name = form.courtname.data
         court.address = form.address.data
@@ -40,11 +38,11 @@ def updateCourt():
         court.description = form.description.data
         db.session.commit()
         flash('Your court has been updated!', 'success')
-        return render_template('/index.html')
-    elif request.method == 'Get':
+         return render_template('/index.html')
+     elif request.method == 'Get':
         form.description.data = court.description
         form.courtname.data = court.name
         form.address.data = court.address
         form.lights.data = court.lights
         form.membership.data= court.membership_required
-    return render_template('create_court.html', title='UpdateCourt', form=form, legend ='Update Post')
+        return render_template('create_court.html', title='UpdateCourt', form=form, legend ='Update Post')
