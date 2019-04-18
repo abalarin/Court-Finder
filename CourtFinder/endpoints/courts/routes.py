75d30ef3627de0c029ca7a3f11ea6ad4a6488466
abalarin <abalarin@outlook.com>
2019-04-18 10:46:34 -0400

    dynamically add courts

--- CourtFinder/endpoints/courts/routes.py
+++ CourtFinder/endpoints/courts/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request
 from CourtFinder.models.courts import Court
 from CourtFinder.endpoints.courts.forms import CourtSearch
 
+import json
+
 courts = Blueprint('courts', __name__)
 
 
@@ -23,4 +25,20 @@ def list_courts():
 
 @courts.route("/map")
 def map_view():
-    return render_template('courts/map.html')
+    courts_query = Court.query.all()
+
+    # This is a list comprehension - it works exactly the same as the for loop below. I went with the for loop for a better readability
+
+    # courts = {court.id :{"name" : court.name, "latlng":{ "lat": float(court.latitude), "lng":float(court.longitude)}} for court in courts}
+
+    courts = {}
+    for court in courts_query:
+        courts[court.id] = {
+            "name": court.name,
+            "latlng" : {
+                "lat" : float(court.latitude),
+                "lng" : float(court.longitude)
+            }
+        }
+        
+    return render_template('courts/map.html', courts=json.dumps(courts))
--- CourtFinder/static/scripts/map.js
+++ CourtFinder/static/scripts/map.js
@@ -1,21 +1,20 @@
 var map;
 
-function initMap(coords) {
+function initMap(courts) {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: 39.7088028,
       lng: -75.1188798
     },
-    zoom: 8
+    zoom: 12
   });
-    var myLatLng = [{lat: 39.7050303, lng: -75.0995315}, {lat: 39.7148264, lng: -75.1314857}, {lat: 39.699367, lng: -75.1161549}, {lat: 39.7256851, lng: -75.1172171}];
-    var marker, i;
-        for(i = 0; i < myLatLng.length; i++) {
-            marker = new google.maps.Marker({
-                position: myLatLng[i],
-                map: map,
-                title: ("Pin" + i)
-            });
-            marker.setMap(map);
-        }
+  var i;
+  for(var key in courts){
+    marker = new google.maps.Marker({
+      position: courts[key].latlng,
+      map: map,
+      title: ("Pin" + i)
+    });
+    marker.setMap(map);
+  }
 }
