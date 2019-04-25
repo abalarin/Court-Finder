f26a9f8d6e9491f39f687e819e2299d40f81e56a
abalarin <abalarin@outlook.com>
2019-04-25 13:04:35 -0400

    form registration on index

--- CourtFinder/endpoints/main/routes.py
+++ CourtFinder/endpoints/main/routes.py
@@ -1,15 +1,43 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user
+from passlib.hash import sha256_crypt
 
 from CourtFinder import db
-from CourtFinder.models.courts import Court
+from CourtFinder.endpoints.users.forms import RegistrationForm
+from CourtFinder.models.users import User
+from CourtFinder.endpoints.users.utils import user_exsists
+
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    db.create_all()
-    return render_template('index.html')
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        # Create user object to insert into SQL
+        hashed_pass = sha256_crypt.encrypt(str(form.password.data))
+
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_pass)
+
+        # Insert new user into SQL
+        if user_exsists(new_user.username, new_user.email):
+            flash('User already exsists!', 'danger')
+            return render_template('index.html', form=form)
+        else:
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Init session vars
+            login_user(new_user)
+
+            return render_template('users/profile.html', user=new_user)
+
+    return render_template('index.html', form=form)
 
 
 @main.app_errorhandler(401)
